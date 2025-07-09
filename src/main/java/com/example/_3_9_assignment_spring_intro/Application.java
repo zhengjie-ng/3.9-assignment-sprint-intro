3884429532c4cd5853faefcf2b0e21ed846ba6d4
Ng Zheng Jie <zhengjie.zjie@gmail.com>
2025-07-09 15:43:47 +0800

    Assignment done

--- src/main/java/com/example/_3_9_assignment_spring_intro/Application.java
+++ src/main/java/com/example/_3_9_assignment_spring_intro/Application.java
@@ -3,6 +3,7 @@ package com.example._3_9_assignment_spring_intro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
 
